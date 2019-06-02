cc90ef7e92a17710a8f70cedaedfb8464785dda0
bursalma <bursalma@gmail.com>
2019-06-01 21:56:15 -0400

    database manipulation

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.use(express.static('./public'));
 
 todoController(app);
 
-app.listen(3000);
-console.log('You are listening to port 3000');
+app.listen(8080);
+console.log('You are listening to port 8080');
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,22 +1,38 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var data = [{item: 'get milk'},{item: 'walk dog'},{item: 'write code'}];
+mongoose.connect('mongodb+srv://root:root@todo-u83ht.mongodb.net/test?retryWrites=true&w=majority')
+
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo',todoSchema);
+
+//var data = [{item: 'get milk'},{item: 'walk dog'},{item: 'write code'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
 
     app.get('/todo', function(req, res){
-        res.render('todo', {todos: data});
+        Todo.find({}, function(err, data){
+            if (err) throw error;
+            res.render('todo', {todos: data});
+        });
     });
 
     app.post('/todo', urlencodedParser, function(req, res){
-        data.push(req.body);
-        res.json(data);
+        var newTodo = Todo(req.body).save(function(err, data){
+            if (err) throw err;
+            res.json(data);
+        });
     });
 
-    app.delete('/todo', function(req, res){
-
+    app.delete('/todo/:item', function(req, res){
+        Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+            if (err) throw err;
+            res.json(data);
+        });
     });
 
 };
